azunite/liblangtag

commit 873a71abe30b023ebd04d26a832bb250807a84c6

minor improvement

liblangtag/lt-extension.c
@@ -406,14 +406,13 @@ lt_extension_compare(const lt_extension_t *v1,
 
 	if (v1 == v2)
 		return TRUE;
-
-	if ((v1 && v1->extensions[LT_MAX_EXT_MODULES - 1]) ||
-	    (v2 && v2->extensions[LT_MAX_EXT_MODULES - 1]))
-		return TRUE;
-
-	if ((!v1 && v2) ||
-	    (v1 && !v2))
+	if (!v1 || !v2)
 		return FALSE;
+
+	/* matching the wildcard each other */
+	if (v1->extensions[LT_MAX_EXT_MODULES - 1] ||
+	    v2->extensions[LT_MAX_EXT_MODULES - 1])
+		return TRUE;
 
 	for (i = 0; i < LT_MAX_EXT_MODULES - 2; i++) {
 		if ((!v1->extensions[i] && v2->extensions[i]) ||

commit 9f9713ad1d4a6deea7d6aa6b4ef54d197680cfcd

Fix a regression issue introduced by 873a71abe30b023ebd04d26a832bb250807a84c6

liblangtag/lt-extension.c
@@ -406,13 +406,12 @@ lt_extension_compare(const lt_extension_t *v1,
 
 	if (v1 == v2)
 		return TRUE;
+	/* matching the wildcard each other */
+	if ((v1 && v1->extensions[LT_MAX_EXT_MODULES - 1]) ||
+	    (v2 && v2->extensions[LT_MAX_EXT_MODULES - 1]))
+		return TRUE;
 	if (!v1 || !v2)
 		return FALSE;
-
-	/* matching the wildcard each other */
-	if (v1->extensions[LT_MAX_EXT_MODULES - 1] ||
-	    v2->extensions[LT_MAX_EXT_MODULES - 1])
-		return TRUE;
 
 	for (i = 0; i < LT_MAX_EXT_MODULES - 2; i++) {
 		if ((!v1->extensions[i] && v2->extensions[i]) ||