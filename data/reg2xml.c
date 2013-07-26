582e487987b78f5b96829110c8724723af59244d
Eike Rathke <erack@redhat.com>
2013-07-26 16:54:21 +0200

    Revert "do not specify a document encoding to avoid garbage conversion"
    This reverts commit 9fb9d1baea38076076dad1c237b04461b6e432ee.
    
    71e2b41a366ecd150700025058364c7dbf3576a7 fixed that.

--- data/reg2xml.c
+++ data/reg2xml.c
@@ -193,6 +193,7 @@ _output_xml(const char *filename,
 	lt_bool_t retval;
 
 	doc = xmlNewDoc((const xmlChar *)"1.0");
+	doc->encoding = xmlStrdup((const xmlChar *)"UTF-8");
 	root = xmlNewDocNode(doc, NULL,
 			     (const xmlChar *)"registry",
 			     NULL);
