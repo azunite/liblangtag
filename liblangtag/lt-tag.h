3cc56cf32c006e7e8445c3ba49ffc128c646c88d
David Tardon <dtardon@redhat.com>
2016-03-23 14:07:48 +0100

    enum must be declared before it's used

--- liblangtag/lt-tag.h
+++ liblangtag/lt-tag.h
@@ -37,6 +37,19 @@ LT_BEGIN_DECLS
  */
 typedef struct _lt_tag_t	lt_tag_t;
 
+enum _lt_tag_filter_t {
+	LT_TAG_FILTER_NONE          = 0,
+	LT_TAG_FILTER_LANGUAGE      = 1 << 0,
+	LT_TAG_FILTER_EXTLANG       = 1 << 1,
+	LT_TAG_FILTER_SCRIPT        = 1 << 2,
+	LT_TAG_FILTER_REGION        = 1 << 3,
+	LT_TAG_FILTER_VARIANT       = 1 << 4,
+	LT_TAG_FILTER_EXTENSION     = 1 << 5,
+	LT_TAG_FILTER_PRIVATEUSE    = 1 << 6,
+	LT_TAG_FILTER_GRANDFATHERED = 1 << 7,
+	LT_TAG_FILTER_ALL           = -1
+};
+
 /**
  * lt_tag_filter_t:
  * @LT_TAG_FILTER_NONE: all of objects filtered.
@@ -54,19 +67,6 @@ typedef struct _lt_tag_t	lt_tag_t;
  * to allow only given tags to be the outcome.
  */
 typedef enum _lt_tag_filter_t	lt_tag_filter_t;
-
-enum _lt_tag_filter_t {
-	LT_TAG_FILTER_NONE          = 0,
-	LT_TAG_FILTER_LANGUAGE      = 1 << 0,
-	LT_TAG_FILTER_EXTLANG       = 1 << 1,
-	LT_TAG_FILTER_SCRIPT        = 1 << 2,
-	LT_TAG_FILTER_REGION        = 1 << 3,
-	LT_TAG_FILTER_VARIANT       = 1 << 4,
-	LT_TAG_FILTER_EXTENSION     = 1 << 5,
-	LT_TAG_FILTER_PRIVATEUSE    = 1 << 6,
-	LT_TAG_FILTER_GRANDFATHERED = 1 << 7,
-	LT_TAG_FILTER_ALL           = -1
-};
 
 lt_tag_t                 *lt_tag_new                         (void);
 lt_tag_t                 *lt_tag_ref                         (lt_tag_t       *tag);
